591e20a018de320542a185daff72ba0e919c14f4
BrianOtto <brian.otto@zoho.com>
2021-02-26 08:30:54 -0800

    Revert "Removed support for nelns"
    This reverts commit 1792b20f7e41175cd46aa90d97ea3cf45759dd16.

--- build.sh
+++ build.sh
@@ -33,6 +33,12 @@ fi
         ( cd nel; git pull )
     fi
 
+    if [ ! -d nelns ]; then
+        git clone https://github.com/anishin-mmorpg/ryzom-nelns.git nelns
+    else
+        ( cd nelns; git pull )
+    fi
+
     if [ ! -d web ]; then
         git clone https://github.com/anishin-mmorpg/ryzom-web.git web
     else
